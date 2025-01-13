27ca55d613e0c921b47a5baa9383b72e0a54a2d2
Vladimer Grigalashvili <lado.grigalashvili@gmail.com>
2025-01-13 09:34:44 +0400

    fking comments :))

--- internal/model/attachment.go
+++ internal/model/attachment.go
@@ -13,9 +13,10 @@ type Attachment struct {
 	CreatedAt time.Time // The timestamp when the record was created.
 	UpdatedAt time.Time // The timestamp when the record was last updated.
 	DeletedAt time.Time // Soft delete field with an index for querying.
-	Name      string    `json:"name"`
-	Type      string    `json:"type"`
-	Size      int       `json:"size"`
-	// OwnerID   uuid.UUID `json:"owner_id"`
+
+	Name string `json:"name"` // The name of the attachment.
+	Type string `json:"type"` // The type of the attachment.
+	Size int    `json:"size"` // The size of the attachment.
+	// OwnerID   uuid.UUID `json:"owner_id"`  // The owner of the attachment.
 
 }
--- internal/model/task.go
+++ internal/model/task.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Task struct {
-	ID        uuid.UUID `json:"id"`
+	ID        uuid.UUID `json:"id"` // The unique identifier of the task.
 	CreatedAt time.Time // The timestamp when record was created.
 	UpdatedAt time.Time // The timestamp when the record was last updated.
 	DeletedAt time.Time // Soft delete field with an index for querying.
 
-	Public      bool   `json:"public"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Location    string `json:"location"`
-	Address     string `json:"address"`
-	Deadline    string `json:"deadline"`
-	Budget      int    `json:"budget"`
-	Status      string `json:"status"`
+	Public      bool   `json:"public"`      // Indicates if the task is public or private.
+	Title       string `json:"title"`       // Title of the task
+	Description string `json:"description"` // Description of the task
+	Location    string `json:"location"`    // Location of the task
+	Address     string `json:"address"`     // Address of the task
+	DeadLine    string `json:"dead_line"`   // Deadline of the task
+	Budget      int    `json:"budget"`      // Budget of the task
+	Status      string `json:"status"`      // Status of the task
 }
